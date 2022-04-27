32b478421f94ad3897f7c781bac37f066ea8c3e6
ullaer <104505658+ullaer@users.noreply.github.com>
2022-04-27 16:09:19 +0300

    Add files via upload

---
+++ CSS-3D/README.md
@@ -0,0 +1,5 @@
+# CSS 3D - Scrolling on the z-axis Demo
+
+[https://vinceumo.github.io/devNotes/CSS/css-3d-scrolling-on-the-z-axis/](https://vinceumo.github.io/devNotes/CSS/css-3d-scrolling-on-the-z-axis/)
+
+![Demo gif](https://vinceumo.github.io/devNotes/images/css3d-z-scroll-demo.gif)
---
+++ index.js
@@ -0,0 +1,155 @@
+let cards = [];
+
+const perspectiveOrigin = {
+  x: parseFloat(
+    getComputedStyle(document.documentElement).getPropertyValue(
+      "--scenePerspectiveOriginX"
+    )
+  ),
+  y: parseFloat(
+    getComputedStyle(document.documentElement).getPropertyValue(
+      "--scenePerspectiveOriginY"
+    )
+  ),
+  maxGap: 10
+};
+
+function inView(id){ //The function to check if ID is in view, returns TRUE/FALSE
+  var object = document.getElementById(id);
+  var y = object.getBoundingClientRect().y
+  //console.log(object.getBoundingClientRect().y, window.innerHeight)
+  if( y < 0 ){
+      return true;
+  }else{
+      return false;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  window.addEventListener("mousemove", moveLetters);
+  window.addEventListener("scroll", function() {
+    if(inView("card-viewport")){
+      document.documentElement.style.setProperty("--viewportDisplay", "block");
+      moveCamera();
+    }  
+    else{
+      document.documentElement.style.setProperty("--viewportDisplay", "none");
+    } 
+  }
+  );
+  window.addEventListener("mousemove", moveCameraAngle);
+  setSceneHeight();
+});
+
+function moveCamera() {
+  document.documentElement.style.setProperty("--cameraZ", window.pageYOffset - (window.innerHeight * 2)); //Minus the height of content above!
+}
+
+function setSceneHeight() {
+  const numberOfItems = 20 //cards.length;
+  const itemZ = parseFloat(
+    getComputedStyle(document.documentElement).getPropertyValue("--itemZ")
+  );
+  const scenePerspective = parseFloat(
+    getComputedStyle(document.documentElement).getPropertyValue(
+      "--scenePerspective"
+    )
+  );
+  const cameraSpeed = parseFloat(
+    getComputedStyle(document.documentElement).getPropertyValue("--cameraSpeed")
+  );
+
+  const height =
+    window.innerHeight +
+    scenePerspective * cameraSpeed +
+    itemZ * cameraSpeed * numberOfItems;
+
+  document.documentElement.style.setProperty("--viewportHeight", height);
+}
+
+function moveCameraAngle(event) { //Changes perspective: moves elements according to mouse coordinates
+  const xGap = //Difference between center and mouseX in %
+    (((event.clientX - window.innerWidth / 2) * 100) /
+      (window.innerWidth / 2)) *
+    -1;
+  const yGap = //Difference between center and mouseY in %
+    (((event.clientY - window.innerHeight / 2) * 100) /
+      (window.innerHeight / 2)) *
+    -1;
+  const newPerspectiveOriginX =
+    perspectiveOrigin.x + (xGap * perspectiveOrigin.maxGap) / 100;
+  const newPerspectiveOriginY =
+    perspectiveOrigin.y + (yGap * perspectiveOrigin.maxGap) / 100;
+
+  document.documentElement.style.setProperty(
+    "--scenePerspectiveOriginX",
+    newPerspectiveOriginX
+  );
+  document.documentElement.style.setProperty(
+    "--scenePerspectiveOriginY",
+    newPerspectiveOriginY
+  );
+}
+
+//Iframe
+// var card = document.getElementsByClassName("card");
+// var iframe = document.getElementById("iframe");
+// for (var i = 0 ; i < 20; i++) { //When card is clicked, show the iframe
+//   card[i].addEventListener("click", function(){
+//     this.classList.toggle('open');
+//     document.body.classList.toggle('no-scroll');
+//     document.documentElement.style.setProperty('--scenePerspective', 100);
+//     // iframe.classList.toggle('hide')
+//   })
+// }
+
+// function cardClose(){
+//   console.log("x");
+//   document.documentElement.style.setProperty('--scenePerspective', 1);
+//   moveCamera();
+//   setSceneHeight();
+// }
+
+var card = document.getElementsByClassName("card");
+var iframe = document.getElementById("iframe");
+var button = document.getElementById("buttonClose");
+var mainBody = document.getElementById("main-body");
+for (var i = 0 ; i < 20; i++) { //When card is clicked, show the iframe
+  card[i].addEventListener("click", function(){
+    iframe.classList.toggle('hide');
+    button.classList.toggle('hide');
+    mainBody.classList.toggle('no-scroll');
+  })
+  card[i].addEventListener("mouseover", function(){
+    iframe.style.transform="translateY(10px)";
+    console.log("hover")
+  })
+}
+function buttonClose(){
+  var d = window.parent.document;
+  var iframe = d.getElementById('iframe');
+  iframe.classList.toggle('hide');
+  button.classList.toggle('hide');
+  d.body.classList.toggle('no-scroll');
+}
+
+//Hero image: letters move with mouse    
+function moveLetters(event) { //Changes perspective: moves elements according to mouse coordinates
+  const xGap = //Difference between center and mouseX in %
+    (((event.clientX - window.innerWidth / 2) * 100) /
+      (window.innerWidth / 2)) * -1;
+  const yGap = //Difference between center and mouseY in %
+    (((event.clientY - window.innerHeight / 2) * 100) /
+      (window.innerHeight / 2)) * -1;
+  const newvalueX = (100 + xGap) /6
+  const newvalueY = (100 + yGap) /10
+  const newsize = 100 - yGap
+
+  console.log(newvalueX,newvalueY)
+
+  var clips = document.getElementsByClassName("clip")
+  for(let i = 0; i < 3; i++){
+    clips[i].style.backgroundPosition=(newvalueX + "px " + newvalueY + "px")
+    // clips[i].style.backgroundSize=(newvalueY + "%")
+  }
+}
